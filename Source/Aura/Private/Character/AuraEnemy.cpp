a0d6c609c0632eaad42976bccb951fc166160f16
Z.Fekete <fekzol.13ker@gmail.com>
2024-12-28 20:52:38 +0100

    Post Process Volume enemy highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,13 +2,33 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
+
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
+	USkeletalMeshComponent* Mesh = GetMesh();
+	
+	Mesh->SetRenderCustomDepth(true);
+	Mesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	
 	bIsHighlighted = true;
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
+	USkeletalMeshComponent* Mesh = GetMesh();
+	
+	Mesh->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
+	
 	bIsHighlighted = false;
 }
